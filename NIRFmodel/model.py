772ef84b3182bf6be948e7e9477527d8ac2a2eff
Suraj Sahu <surajsahu9658@gmail.com>
2023-11-02 13:49:25 +0530

    Update and rename tlrLearn.py to model.py

--- NIRFmodel/model.py
+++ NIRFmodel/model.py
@@ -4,6 +4,7 @@ import json
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import math
 
 data = pd.read_csv("dataNIRF.csv") #Idhar tera csv file hai
 
